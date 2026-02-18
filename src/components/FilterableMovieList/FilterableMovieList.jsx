25114d7a300f29282e8b3c5d29e51b75c447b44a
AnthonyEsber <Anthony.Esber@cognizant.com>
2026-02-18 11:27:41 +0200

    feat: added filtering options through URLs

--- src/components/FilterableMovieList/FilterableMovieList.jsx
+++ src/components/FilterableMovieList/FilterableMovieList.jsx
@@ -2,12 +2,28 @@ import { useState } from 'react';
 import MovieList from '../MovieList/MovieList';
 import SearchFilters from '../SearchFilters/SearchFilters';
 import style from './FilterableMovieList.module.css';
+import { useSearchParams } from 'react-router';
 
 function FilterableMovieList({ movies }) {
   const genres = [...new Set(movies.map((movie) => movie.genre))];
-  const [filterTitle, setFilterTitle] = useState('');
-  const [selectedGenre, setSelectedGenre] = useState('');
-  const [sortBy, setSortBy] = useState('default');
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const filterTitle = searchParams.get('search') ?? '';
+  const selectedGenre = searchParams.get('genre') ?? '';
+  const sortBy = searchParams.get('order') ?? '';
+
+  const updateParam = (key, value) => {
+    setSearchParams((prev) => {
+      const next = new URLSearchParams(prev);
+      if (value) {
+        next.set(key, value);
+      } else {
+        next.delete(key);
+      }
+      return next;
+    });
+  };
 
   return (
     <div className={style.movieContainer}>
@@ -15,10 +31,10 @@ function FilterableMovieList({ movies }) {
         filterTitle={filterTitle}
         genres={genres}
         selectedGenre={selectedGenre}
-        onFilterTitleChange={setFilterTitle}
-        onSelectedGenreChange={setSelectedGenre}
+        onFilterTitleChange={(value) => updateParam('search', value)}
+        onSelectedGenreChange={(value) => updateParam('genre', value)}
         sortBy={sortBy}
-        onSortChange={setSortBy}
+        onSortChange={(value) => updateParam('order', value)}
       />
       <MovieList
         selectedGenre={selectedGenre}
